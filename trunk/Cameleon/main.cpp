6be1d893bd93d771ed65fb9a593fe2682ef18c7c
xpierro@gmail.com <xpierro@gmail.com@bc14e4aa-ce04-fbfb-5b67-d798b815e5a7>
2011-06-03 19:25:03 +0000

    First commit

---
+++ trunk/Cameleon/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: pierre
+ *
+ * Created on 4 juin 2011, 21:20
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
